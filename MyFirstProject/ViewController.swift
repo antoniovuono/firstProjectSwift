27802ea44fcc731be5cc369809916c4045ba65ee
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-12-08 16:38:32 -0300

    first commit

--- MyFirstProject/ViewController.swift
+++ MyFirstProject/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var sliderLabel: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func changeFontSize(_ sender: Any) {
+        titleLabel.font = UIFont(name: "helvetica", size: CGFloat(sliderLabel.value))
+        
+        counterLabel.text = String(sliderLabel.value)
+    }
 }
-
